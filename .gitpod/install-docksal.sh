9d4acbac19b337346566b252fc3a8f59295de34a
shaal <ofer@3paces.com>
2021-05-19 17:18:00 +0000

    fixing script

--- .gitpod/install-docksal.sh
+++ .gitpod/install-docksal.sh
@@ -47,5 +47,5 @@ fi
 
 sudo mkdir -p /usr/local/bin &&
 	sudo curl -fsSL "https://raw.githubusercontent.com/docksal/docksal/${DOCKSAL_VERSION}/bin/fin?r=${RANDOM}" -o /usr/local/bin/fin &&
-	sudo chmod +x /usr/local/bin/fin &&
+	sudo chmod +x /usr/local/bin/fin
 	# fin update
